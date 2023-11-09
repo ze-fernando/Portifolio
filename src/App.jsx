57926dbcd1ed1db277d68f218382b1d3a39f5a77
ze-fernando <jfenginner935@gmail.com>
2023-11-08 22:26:10 -0300

    feat: connect api github

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
+import React, { useState, useEffect } from 'react';
 import './static/style.css';
 import zeca from './static/zeca.jpg'
 import wpp from './static/whatsapp.png';
 import linke from './static/linkedin.png';
 import git from './static/github.png';
 
-
+function lang(lang) {
+  switch (lang) {
+    case "C#":
+      return <img alt='C#' src='https://img.shields.io/badge/C%23-239120?style=for-the-badge&logo=c-sharp&logoColor=white'/>;
+    case "JavaScript":
+      return <img alt='JavaScript' src='https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black'/>;
+    case "Python":
+      return <img alt='Python' src='https://img.shields.io/badge/Python-14354C?style=for-the-badge&logo=python&logoColor=white'/>;
+    case "Java":
+      return <img alt='Java' src='https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white'/>;
+    case "C":
+      return <img alt='C' src='https://img.shields.io/badge/C-00599C?style=for-the-badge&logo=c&logoColor=white'/>;
+    case "CSS":
+      return <img alt='CSS3' src='https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white'/>;
+    case "SQL":
+      return <img alt='SQL' src='https://img.shields.io/badge/SQL-07405E?style=for-the-badge&Color=white'/>;
+    default:
+      break;
+  }
+}
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/ze-fernando/repos')
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
+
   return (
   <div className='body'>
     <div className="boxTitle">
@@ -23,7 +52,7 @@ function App() {
         <img className="tag" align='center' alt='C#' src='https://img.shields.io/badge/C%23-239120?style=for-the-badge&logo=c-sharp&logoColor=white'/>
         <img className="tag" align='center' alt='Python' src='https://img.shields.io/badge/Python-14354C?style=for-the-badge&logo=python&logoColor=white'/>
         <img className="tag" align='center' alt='Java' src='https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white'/>
-        <img className="tag" align='center' alt='Typescript' src='https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white'/>
+        <img className="tag" align='center' alt='JavaScript' src='https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black'/>
         <img className="tag" align='center' alt='MongoDB' src='https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white'/>
         <img className="tag" align='center' alt='SQLite3' src='https://img.shields.io/badge/SQLite-07405E?style=for-the-badge&logo=sqlite&logoColor=white'/>
         <img className="tag" align='center' alt='MySQL' src='https://img.shields.io/badge/MySQL-005C84?style=for-the-badge&logo=mysql&logoColor=white'/>
@@ -48,73 +77,23 @@ function App() {
       </div>
     </div>
 
-      <h2 className="title">Projetos</h2>
+    <h2 className="title">Projetos</h2>
     <div className="containerProjects1">
-      <a href="https://github.com/ze-fernando/TodoApp-Django" target="_blank" rel="noreferrer">
-        <div className="divProj">
-          <h1>Todo app - Django</h1>
-            <div className="boximg">
-              <img src="https://img.shields.io/badge/Python-14354C?style=for-the-badge&logo=python&logoColor=white" alt="Python"/>
-              <img src="https://img.shields.io/badge/Django-092E20?style=for-the-badge&logo=django&logoColor=white" alt="Django"/>
-              <img src="https://img.shields.io/badge/SQLite-07405E?style=for-the-badge&logo=sqlite&logoColor=white" alt="SQLite"/>
+    {data.map(rep =>{
+      if (rep.language !== null){
+        return(          
+          <a href={rep.html_url} target="_blank" rel="noreferrer">
+            <div className="divProj">
+              <h1>{rep.name}</h1>
+              <p>{rep.description}</p>
+              <div className="boximg">
+                {lang(rep.language)}          
+              </div>
             </div>
-          <p>Todo list feito em Django, utilizei para este projeto o banco de dados padrão do django o SQLite3, 
-            e pude compreender como funciona um banco de dados, bem como as autenticações e cadastro são feitas.</p>
-        </div>
-      </a>
-      
-      <a href="https://github.com/ze-fernando/node-API" target="_blank" rel="noreferrer">
-        <div className="divProj">
-          <h1>JWT - Node</h1>
-            <div className="boximg">
-              <img src="https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white" alt="Node"/>
-              <img src="https://img.shields.io/badge/Express.js-404D59?style=for-the-badge" alt="Express"/>
-              <img src="https://img.shields.io/badge/MySQL-005C84?style=for-the-badge&logo=mysql&logoColor=white" alt="MySQL"/>
-              <img src="https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white" alt="Docker"/>
-            </div>
-          <p>Api feita em NodeJS, neste projeto utilizei o Express.js para construir minha API de
-            autenticação, pude trabalhar com Docker-compose, JWT, autorizações, encripitação no
-          banco de dados e comparações de hash.
-          </p>
-      </div>
-      </a>
-
-      <a href="https://github.com/ze-fernando/EmailSender-csharp" target="_blank" rel="noreferrer">
-        <div className="divProj">
-          <h1>Email sender - .Net</h1>
-          <div className="boximg">
-            <img src="https://img.shields.io/badge/C%23-239120?style=for-the-badge&logo=c-sharp&logoColor=white" alt="C#"/>
-            <img src="https://img.shields.io/badge/.NET-5C2D91?style=for-the-badge&logo=.net&logoColor=white" alt=".Net"/>
-          </div>
-            <p>Api para envio de emails utilizando C# e dotnet, pude compreender ao fazer este projeto, o processo de criação, construção e manuntenção de Api's 
-              utlizando a platafoma .Net, bem como a estrutura do projeto apiweb e arquitetura no C#</p>
-      </div>
-      </a>
-
-      <a href="https://github.com/ze-fernando/LibraryAPI-spring" target="_blank" rel="noreferrer">
-        <div className="divProj">
-          <h1>Library API - Spring</h1>
-          <div className="boximg">
-            <img src="https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white"  alt="Java"/>
-            <img src="https://img.shields.io/badge/Spring-6DB33F?style=for-the-badge&logo=spring&logoColor=white"  alt="Spring"/>
-            <img src="https://img.shields.io/badge/MySQL-005C84?style=for-the-badge&logo=mysql&logoColor=white" alt="MySQL"/>
-            <img src="https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white" alt="Docker"/>
-          </div>
-            <p>Uma api para um sistema de biblioteca, onde pude aprender os conceitos do JPA, aprimorar meus conhecimentos 
-            em banco de dados e relacionar tabelas com chave estrangeira, utilizei Docker para meu DB, e Spring Boot para construir a Api</p>
-      </div>
-      </a>
-
-      <a href="https://github.com/ze-fernando/sorting-algorithm" target="_blank" rel="noreferrer">
-        <div className="divProj">
-          <h1>Sorting Algorithm</h1>
-          <div className="boximg">
-            <img src="https://img.shields.io/badge/C-00599C?style=for-the-badge&logo=c&logoColor=white" alt="C-lang"/>
-          </div>
-          <p>Algoritmo de ordenação de listas, projeto feito em C-lang, onde pude praticar minha logica de programação e compreender o processo do famoso 
-              metodo "Sort()", neste projeto fui capaz ter uma base do funcionamento deste algoritimo tão utilizado no nosso dia a dia.</p>
-        </div>
-      </a>
+          </a>  
+        )
+      }
+      })}
     </div>
 
     <h2 className="title">Contate-me via Email</h2>
@@ -136,7 +115,7 @@ function App() {
         </div>
       </a> 
       <a href="https://github.com/ze-fernando" target="_blank" rel="noreferrer">
-        <div className='divIcon'>
+         <div className='divIcon'>
           <img src={git} alt="github" className="icon"/>
           Github
         </div>
